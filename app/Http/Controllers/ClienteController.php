7e4f1317eae40c007e2e1726ddc7850f9ec53b88
gonzalo <renovado16@gmail.com>
2024-08-12 19:10:54 -0500

    envio en json

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -13,7 +13,7 @@ class ClienteController extends Controller
     public function index()
     {
         $clientes = Cliente::all();
-        return $clientes;
+        return response()->json($clientes);
     }
 
     /**
